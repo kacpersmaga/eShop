d2c99d04df7b87b917f1d624abd347ca23efcecf
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2025-01-13 01:47:28 +0100

    Dockerized .NET application and database, added default appsettings.json with database and Azure Blob Storage connection strings

--- eShop/Program.cs
+++ eShop/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddScoped<BlobStorageService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
