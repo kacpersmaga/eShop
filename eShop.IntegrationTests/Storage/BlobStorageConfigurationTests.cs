679b7cd5c4cb8195d85ef20ed6d96ff0fcd7506a
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-02-19 01:45:38 +0100

    test(storage): add integration test for BlobStorageConfiguration DI registration

---
+++ eShop.IntegrationTests/Storage/BlobStorageConfigurationTests.cs
@@ -0,0 +1,23 @@
+using Azure.Storage.Blobs;
+using eShop.Services.Interfaces;
+using IntegrationTests.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntegrationTests.Storage;
+
+public class BlobStorageConfigurationTests(CustomWebApplicationFactory factory)
+    : IClassFixture<CustomWebApplicationFactory>
+{
+    [Fact]
+    public void Application_ShouldRegisterBlobStorageServices()
+    {
+        // Arrange
+        using var scope = factory.Services.CreateScope();
+        var provider = scope.ServiceProvider;
+
+        // Act & Assert
+        Assert.NotNull(provider.GetService<BlobServiceClient>());
+        Assert.NotNull(provider.GetService<IBlobStorageService>());
+        Assert.NotNull(provider.GetService<IBlobStorageServiceWrapper>());
+    }
+}
