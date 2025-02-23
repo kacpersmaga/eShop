587ae1149b13a938179ae1d17001cb8b4094c33a
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-02-23 02:23:54 +0100

    feat(logging): enhance logging configuration to support testing with TestCorrelator
    - Modified `ConfigureSerilog` to conditionally add `TestCorrelator` sink in the test environment.
    - Added a new test `ConfigureSerilog_ShouldInitializeLoggerAndWriteToConsole` to verify logging behavior better than the previous test.

--- eShop/Extensions/Logging/LoggingConfiguration.cs
+++ eShop/Extensions/Logging/LoggingConfiguration.cs
@@ -6,11 +6,17 @@ public static class LoggingConfiguration
 {
     public static void ConfigureSerilog(this IHostBuilder builder)
     {
-        Log.Logger = new LoggerConfiguration()
+        var loggerConfiguration = new LoggerConfiguration()
             .Enrich.FromLogContext()
             .WriteTo.Console()
-            .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
+            .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day);
+
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Test")
+        {
+            loggerConfiguration.WriteTo.TestCorrelator();
+        }
+
+        Log.Logger = loggerConfiguration.CreateLogger();
 
         builder.UseSerilog();
     }
