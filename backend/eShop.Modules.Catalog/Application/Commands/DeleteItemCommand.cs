d1fa8a67d657d84aaadce2bc02052a8a4e492fe7
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-03-27 20:03:09 +0100

    fix: add missing files to project

---
+++ backend/eShop.Modules.Catalog/Application/Commands/DeleteItemCommand.cs
@@ -0,0 +1,6 @@
+﻿using eShop.Shared.Abstractions.Primitives;
+using MediatR;
+
+namespace eShop.Modules.Catalog.Application.Commands;
+
+public record DeleteItemCommand(int ItemId) : IRequest<Result<string>>;
